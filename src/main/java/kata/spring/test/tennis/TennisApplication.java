a6907beb3dfa2974f25e3fd81aed0cd0f6856f40
Khaled Baklouti <khaledbaklouti8@gmail.com>
2019-03-06 16:29:44 +0100

    initialize spring boot project

---
+++ src/main/java/kata/spring/test/tennis/TennisApplication.java
@@ -0,0 +1,21 @@
+package kata.spring.test.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Tennis application entry point class.
+ */
+@SpringBootApplication
+public class TennisApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(TennisApplication.class, args);
+	}
+
+}
