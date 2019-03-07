eecc45d4e8c9212186097f7ba71e33f6119e4e93
Khaled Baklouti <khaledbaklouti8@gmail.com>
2019-03-07 18:04:29 +0100

    fix the scope to manage sets

--- src/main/java/kata/spring/test/tennis/service/SetService.java
+++ src/main/java/kata/spring/test/tennis/service/SetService.java
@@ -3,43 +3,42 @@ package kata.spring.test.tennis.service;
 import kata.spring.test.tennis.config.ApplicationContextHolder;
 import kata.spring.test.tennis.config.ClientBeanFactoryProcessor;
 import kata.spring.test.tennis.models.Game;
+import kata.spring.test.tennis.models.Set;
 import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 /**
- * The Game service.
+ * The Set service.
  */
 @Service
-public abstract class GameService {
+public abstract class SetService {
 
 
     /**
-     * this abstract method will return the bean Game, depending on the session.
+     * this abstract method will return the bean Set, depending on the session.
      *
-     * @return the game active in this session
+     * @return the set active in this session
      */
     @Lookup
-    public abstract Game getGame();
+    public abstract Set getSet();
 
 
     /**
      * adds a point to the player of the selected side.
      *
      * @param side the selected side
-     * @return the game active in this session
+     * @return the set active in this session
      */
-    public Game addPoint(Game.GameSide side) {
-        Game game = getGame();
-        game.incrementPlayerScore(side);
-        return game;
+    public Set addPoint(Game.GameSide side) {
+        Set set = getSet();
+        set.incrementPlayerScore(side);
+        return set;
     }
 
-    public void resetGame() {
+    public void resetSet() {
         ApplicationContextHolder
                 .getApplicationContext()
                 .getBeansOfType(ClientBeanFactoryProcessor.class)
-        .values().forEach(clientBeanFactoryProcessor -> clientBeanFactoryProcessor.getGameScope().remove());
+                .values().forEach(clientBeanFactoryProcessor -> clientBeanFactoryProcessor.getSetScope().remove());
     }
 }
